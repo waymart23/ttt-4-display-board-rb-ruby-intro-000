511c4b9d1d2e0ec2ecec5c6d0b29a88079d1e629
Wayne Martin <waymart23@gmail.com>
2020-04-18 16:11:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,6 @@
 def display_board(board)
-board = [" "," "," "," "," "," "," "," "," "]
+  puts " #{board[1]} | #{board[2]} | #{board[3]} "
+  puts " #{board[4]} | #{board[5]} | #{board[6]} "
+  puts " #{board[7]} | #{board[8]} | #{board[9]} "
 end
+ 
