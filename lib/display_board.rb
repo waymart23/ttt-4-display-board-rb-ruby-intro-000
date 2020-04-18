a773ae425c992c2fe78f55c1f313cdf75468ed39
Wayne Martin <waymart23@gmail.com>
2020-04-18 15:40:15 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,2 @@
-def display_board
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-end
+board = [" "," "," "," "," "," "," "," "," "]
+display_board
